42ea824ebfe8edc50672ae8bf5777d120aec6354
Brandon Fulljames <bfullj@gmail.com>
2024-11-07 19:24:16 +0900

    Add burst receive for udp (#57)

--- pkg/listener/udp.go
+++ pkg/listener/udp.go
@@ -1,11 +1,13 @@
 package listener
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/evertras/cynomys/pkg/constants"
 )
@@ -18,6 +20,8 @@ type UdpListener struct {
 
 type UdpConfig struct {
 	Echo bool
+
+	Burst BurstConfig
 }
 
 func NewUDP(addr net.UDPAddr, cfg UdpConfig) *UdpListener {
@@ -35,6 +39,18 @@ func (l *UdpListener) Addr() string {
 }
 
 func (l *UdpListener) Listen() error {
+	l.mu.RLock()
+	burst := l.cfg.Burst.Window != 0
+	l.mu.RUnlock()
+
+	if burst {
+		return l.listenBurst()
+	}
+
+	return l.listenSingle()
+}
+
+func (l *UdpListener) listenSingle() error {
 	l.mu.RLock()
 	conn, err := net.ListenUDP("udp", &l.addr)
 	l.mu.RUnlock()
@@ -64,6 +80,79 @@ func (l *UdpListener) Listen() error {
 
 		if l.cfg.Echo {
 			log.Printf("Received: %s", strings.ReplaceAll(string(buf), "\n", "\\n"))
+		}
+	}
+}
+
+func (l *UdpListener) listenBurst() error {
+	l.mu.RLock()
+	conn, err := net.ListenUDP("udp", &l.addr)
+	cfg := l.cfg
+	l.mu.RUnlock()
+	if err != nil {
+		return fmt.Errorf("net.ListenUDP: %w", err)
+	}
+	defer conn.Close()
+
+	data := make(chan []byte, 0)
+	errs := make(chan error, 1)
+
+	go func() {
+		buf := make([]byte, maxPacketBytesReceived)
+
+		for {
+			rlen, _, err := conn.ReadFromUDP(buf)
+
+			if err != nil {
+				errs <- fmt.Errorf("conn.ReadFromUDP: %w", err)
+				return
+			}
+
+			data <- buf[:rlen]
+		}
+	}()
+
+	var aggregateData bytes.Buffer
+
+	var timer *time.Timer
+
+	for {
+		select {
+		case rcv := <-data:
+			_, err := aggregateData.Write(rcv)
+			if err != nil {
+				return fmt.Errorf("failed to write to aggregate: %w", err)
+			}
+
+			timer = time.NewTimer(cfg.Burst.Window)
+
+		case err := <-errs:
+			return err
+		}
+
+	TIMEDLOOP:
+		for {
+			select {
+			case rcv := <-data:
+				_, err := aggregateData.Write(rcv)
+				if err != nil {
+					return fmt.Errorf("failed to write to aggregate: %w", err)
+				}
+
+			case err := <-errs:
+				return err
+
+			case <-timer.C:
+				timer = nil
+
+				log.Printf("Read %d bytes", aggregateData.Len())
+
+				if l.cfg.Echo {
+					log.Printf("Received: %s", strings.ReplaceAll(aggregateData.String(), "\n", "\\n"))
+				}
+
+				break TIMEDLOOP
+			}
 		}
 	}
 }
